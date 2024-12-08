e82f8a948481749eef7738a31d1b755a87158567
Senad Mehmeti <senadmehmeti@proton.me>
2024-12-08 02:19:36 +0100

    finished search barin

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
+
 menu.onclick = () =>{
     navbar.classList.toggle('active');  
     menu.classList.toggle('fa-times');
+
 }
+
+window.onscroll = () =>{
+    navbar.classList.toggle('active');  
+    menu.classList.toggle('fa-times');
+    
+}
+
+document.querySelector('#search-icon').onclick = () =>{
+ document.querySelector('#search-form').classList.toggle('active');
+     
+}
+
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active');
+        
+   }
